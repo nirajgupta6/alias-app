d5651c9972841255cbb60edb8f14f0f1c829e7ae
nirajgupta6 <nirajgupta0605@gmail.com>
2024-07-12 12:37:03 +0530

    changes

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -1,13 +1,14 @@
 package com.training.bms;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AlstromAppApplicationTests {
+class  AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		assertEquals (190,200);
 	}
 
 }
